0b50595a4d510e2a5c678369f1dcc14be893edad
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-06-07 09:41:52 +0700

    Add flexible configuration

--- lib/mega_lotto/drawing.rb
+++ lib/mega_lotto/drawing.rb
@@ -3,11 +3,21 @@
 module MegaLotto
   # Drawing business logic
   class Drawing
+    attr_accessor :configuration
+
+    def initialize(configuration = nil)
+      @configuration = configuration if configuration
+    end
+
     def draw
-      Array.new(MegaLotto.configuration.drawing_count) { single_random_draw }
+      Array.new(drawing_count) { single_random_draw }
     end
 
     private
+
+    def drawing_count
+      configuration&.drawing_count || MegaLotto.configuration.drawing_count
+    end
 
     def single_random_draw
       rand(1..60)
